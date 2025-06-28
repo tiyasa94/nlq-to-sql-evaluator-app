6efcda05fc63ab67e585f6f8047704d6e1a96911
Tiyasa Mukherjee <74978788+tiyasa94@users.noreply.github.com>
2025-06-28 11:45:50 +0530

    Add files via upload

---
+++ src/database/load_departments.py
@@ -0,0 +1,9 @@
+DEPARTMENT_DATA = {'departments' : [('dept_no', 'dept_name'),[('d001','Marketing'),
+('d002','Finance'),
+('d003','Human Resources'),
+('d004','Production'),
+('d005','Development'),
+('d006','Quality Management'),
+('d007','Sales'),
+('d008','Research'),
+('d009','Customer Service')] ]}
